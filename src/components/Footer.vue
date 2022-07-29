4b60ba8e13f0c1e63e0d28752c3a19cf4e771b4a
JiangXiaoqiang <jiangtingqiang@gmail.com>
2022-07-29 22:32:20 +0800

    feat: add the company contact information

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,17 +3,16 @@
     <div class="logo">
       <img src="@/assets/img/logo_white.png" alt="logo图">
     </div>
-    <p class="title">公司企业网站模板</p>
+    <!--<p class="title">公司企业网站模板</p>-->
     <p class="address_tel_fax">
-      <span>地址：公司地址公司地址公司地址公司地址</span>
-      <span>Tel：888-888-888</span>
-      <span>Fax：8888-88888888</span>
+      <span>地址：重庆市大足区棠香街道五星大道中段243号1幢22-3</span>
+      <span>Tel：15683761628</span>
     </p>
     <p class="email_wx">
-      <span>邮箱：liyunkun_11@163.com</span>
-      <span>公司微信号：ilyk_gg</span>
+      <span>邮箱：jiangtingqiang@gmail.com</span>
+      <!--<span>公司微信号：jiangxiaoqiang_123456</span>-->
     </p>
-    <p class="copy">Copyright &copy; 2018 - 2019 公司名称公司名称</p>
+    <p class="copy">Copyright &copy; 2021 - 2022 重庆红矮星科技有限公司</p>
   </div>
 </template>
 <script>
